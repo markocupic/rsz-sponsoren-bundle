032b92bcb82db10244cd5613522bc893a795a56d
Marko Cupic <m.cupic@gmx.ch>
2020-08-03 23:55:05 +0200

    development

---
+++ src/Resources/contao/languages/en/tl_module.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of a markocupic Contao Bundle
+ *
+ * @copyright  Marko Cupic 2020 <m.cupic@gmx.ch>
+ * @author     Marko Cupic
+ * @package    RSZ Sponsoren
+ * @license    MIT
+ * @see        https://github.com/markocupic/rsz-sponsoren-bundle
+ *
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_module']['rszSponsorLevel'] = ['Sponsor-Level', 'Geben Sie das Sponsor-Level an.'];
+
+
